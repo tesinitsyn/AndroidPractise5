2852ba5edf4c236ffadce9c973813bc0ec1d2fb2
tesinitsyn <sitiev174@gmail.com>
2023-04-07 23:33:46 +0300

    Added empty fragment classes and exported layouts from previous projects

---
+++ app/src/main/java/com/example/androidpractise5/Registration.java
@@ -0,0 +1,35 @@
+package com.example.androidpractise5;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Registration extends Fragment {
+
+
+    public Registration() {
+        // Required empty public constructor
+    }
+
+    public static Registration newInstance(String param1, String param2) {
+        Registration fragment = new Registration();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_registration, container, false);
+    }
+}
