dcd62b3920ee87d1bfc99a5ea28d198ab54b569a
tesinitsyn <sitiev174@gmail.com>
2023-04-08 10:48:49 +0300

    finished project

--- app/src/main/java/com/example/androidpractise5/Registration.java
+++ app/src/main/java/com/example/androidpractise5/Registration.java
@@ -2,11 +2,20 @@ package com.example.androidpractise5;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Objects;
 
 
 public class Registration extends Fragment {
@@ -16,7 +25,7 @@ public class Registration extends Fragment {
         // Required empty public constructor
     }
 
-    public static Registration newInstance(String param1, String param2) {
+    public static Registration newInstance() {
         Registration fragment = new Registration();
         return fragment;
     }
@@ -31,5 +40,24 @@ public class Registration extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_registration, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+        TextInputEditText name = view.findViewById(R.id.Name);
+        TextInputEditText surname = view.findViewById(R.id.surname);
+        TextInputEditText email = view.findViewById(R.id.email);
+
+        Button submit = view.findViewById(R.id.submit_button);
+
+        submit.setOnClickListener(v ->{
+            Bundle profileInfo = new Bundle();
+            profileInfo.putString("name", Objects.requireNonNull(name.getText()).toString());
+            profileInfo.putString("surname", Objects.requireNonNull(surname.getText()).toString());
+            profileInfo.putString("email", Objects.requireNonNull(email.getText()).toString());
+            Navigation.findNavController(view).navigate(R.id.action_registration_to_profile, profileInfo);
+        });
+
     }
 }
